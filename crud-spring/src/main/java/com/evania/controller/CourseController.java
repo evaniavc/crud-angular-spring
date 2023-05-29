424d8fc82e56b8dc96050b156bb9d5953551de3c
evaniavc <evaniavcarvalho@gmail.com>
2023-05-28 22:33:27 -0300

    Aula 21 - crud-angular

--- crud-spring/src/main/java/com/evania/controller/CourseController.java
+++ crud-spring/src/main/java/com/evania/controller/CourseController.java
@@ -5,8 +5,15 @@ import java.util.List;
 import com.evania.model.Course;
 import com.evania.repository.CourseRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -18,10 +25,20 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    //@RequestMapping(method = RequestMethod.GET)
+    // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    // @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED)
+        //     .body(courseRepository.save(course));
     }
 
 }
